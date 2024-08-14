49e683cc7f93ff4c3ac9825bfb3a7f5a70b80d53
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-08-14 11:41:41 +0200

    Login and Register buttons working as intended, Working their respective containers to be better and dynamic

--- frontend/scripts/buttons.js
+++ frontend/scripts/buttons.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('register-form-container').style.display = 'none';
     document.getElementById('login-form-container').style.display = 'none';
 
     function showLoginForm() {
+        document.getElementById('register-form-container').style.display = 'none';
         document.getElementById('login-form-container').style.display = 'block';
     }
-
-    // Auto-click login button on page load to simulate the login button being pressed
     showLoginForm();
-    document.getElementById('login-btn').focus(); // Set focus on login button
+    document.getElementById('login-btn').focus(); 
 
     document.getElementById('login-btn').addEventListener('click', showLoginForm);
 
     document.getElementById('register-btn').addEventListener('click', function () {
         document.getElementById('login-form-container').style.display = 'none';
-        // Add functionality to show the register form here.
+        document.getElementById('register-form-container').style.display = 'block';
     });
 });
