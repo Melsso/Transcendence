acdce3f177f5a2839bdffa2788397ffede0c14bd
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-08-14 13:09:05 +0200

    90% done with the login and register blocks

--- frontend/scripts/buttons.js
+++ frontend/scripts/buttons.js
@@ -9,7 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
     showLoginForm();
     document.getElementById('login-btn').focus(); 
 
-    document.getElementById('login-btn').addEventListener('click', showLoginForm);
+    document.getElementById('login-btn').addEventListener('click', showLoginForm, function(){
+        document.getElementById('register-form-container').style.display = 'none';
+        document.getElementById('login-form-container').style.display = 'block';
+    });
 
     document.getElementById('register-btn').addEventListener('click', function () {
         document.getElementById('login-form-container').style.display = 'none';
