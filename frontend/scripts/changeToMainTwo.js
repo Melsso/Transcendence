71833d06fd0f2cd1e29000599e344d6ef316acbe
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-08-19 18:00:09 +0200

    Worked a bit more on the main page, still not done

--- frontend/scripts/changeToMainTwo.js
+++ frontend/scripts/changeToMainTwo.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	// Get the login and register buttons
 	const loginButton = document.getElementById('login');
 	const registerButton = document.getElementById('register');
+	const logoutButon = document.getElementById('logout');
 
 	// Add click event listener to the login button
 	loginButton.addEventListener('click', function () {
@@ -19,5 +20,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	registerButton.addEventListener('click', function () {
 		 mainOne.style.display = 'none';
 		 mainTwo.style.display = 'flex';
+	});
+	logoutButon.addEventListener('click', function() {
+		document.getElementById('register-form-container').style.display = 'none';
+		document.getElementById('second-reg-container').style.display = 'none';
+		document.getElementById('login-form-container').style.display = 'block';
+		mainOne.style.display = 'flex';
+		mainTwo.style.display = 'none';
 	});
 });
