86420f4ef679fc2811fda8864953657f586df6f9
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-08-20 18:25:42 +0200

    Main structure for profile info should be done and dynamic, will work more on stat-container

--- frontend/scripts/fetch.js
+++ frontend/scripts/fetch.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const jsonUrl = '../profile.json';
+	const jsonUrl = 'profile.json'; // Adjust this path based on where your JSON file is located
 
 	fetch(jsonUrl)
 		 .then(response => {
@@ -15,10 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
 			  const usernameElement = document.getElementById('username');
 			  usernameElement.textContent = data.username;
 
-			  const progressBarElement = document.querySelector('#exp-bar .progress-bar');
-			  progressBarElement.style.width = `${data.expPercentage}%`;
-			  progressBarElement.textContent = `Level ${data.level} - ${data.expPercentage}%`;
-			  progressBarElement.setAttribute('aria-valuenow', data.expPercentage);
+			  const bioElement = document.getElementById('profile-bio');
+			  bioElement.textContent = data.bio;
+
+			  const progressBarElement1 = document.querySelector('#exp-bar .progress-bar');
+			  progressBarElement1.style.width = `${data.game1.expPercentage}%`;
+			  progressBarElement1.textContent = `Game 1 - Level ${data.game1.level} - ${data.game1.expPercentage}%`;
+			  progressBarElement1.setAttribute('aria-valuenow', data.game1.expPercentage);
+
+			  const progressBarElement2 = document.querySelector('.my-bar-two .progress-bar');
+			  progressBarElement2.style.width = `${data.game2.expPercentage}%`;
+			  progressBarElement2.textContent = `Game 2 - Level ${data.game2.level} - ${data.game2.expPercentage}%`;
+			  progressBarElement2.setAttribute('aria-valuenow', data.game2.expPercentage);
 		 })
 		 .catch(error => {
 			  console.error('Error fetching profile data:', error);
