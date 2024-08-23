ec2952f3c3797b9b322e57f5769a68ed6fbdae01
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-08-23 16:42:36 +0200

    Added functionallity for buttons to switch pages/bodies

--- frontend/scripts/changeToMainTwo.js
+++ frontend/scripts/changeToMainTwo.js
@@ -2,13 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	// Get the mainOne element
 	const mainOne = document.getElementById('mainOne');
 	const mainTwo = document.getElementById('mainTwo');
+	const mainBody = document.getElementById('mainTwo-body');
+	const mainSettings = document.getElementById('setting-page');
+	const mainSLgame = document.getElementById('S&L-page');
+	mainSLgame.style.display = 'none';
 	mainOne.style.display = 'none';
 	mainTwo.style.display = 'flex';
+	mainBody.style.display = 'none'
+	mainSettings.style.display = 'flex';
 
 	// Get the login and register buttons
 	const loginButton = document.getElementById('login');
+	const profileButton = document.getElementById('to-profile');
 	const registerButton = document.getElementById('register');
 	const logoutButon = document.getElementById('logout');
+	const SLButton = document.getElementById('S&L-play');
+	const settingButton = document.getElementById('to-settings');
 
 	// Add click event listener to the login button
 	loginButton.addEventListener('click', function () {
@@ -27,5 +36,22 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.getElementById('login-form-container').style.display = 'block';
 		mainOne.style.display = 'flex';
 		mainTwo.style.display = 'none';
+	});
+
+	settingButton.addEventListener('click', function() {
+		mainSettings.style.display = 'flex';
+		mainBody.style.display = 'none';
+	});
+
+	profileButton.addEventListener('click', function () {
+		mainBody.style.display = 'flex';
+		mainSettings.style.display = 'none';
+		mainSLgame.style.display = 'none';
+	});
+
+	SLButton.addEventListener('click', function() {
+		mainBody.style.display = 'none';
+		mainSettings.style.display = 'none';
+		mainSLgame.style.display = 'flex';
 	});
 });
