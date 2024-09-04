10ec0e6e48af8da296509f5a3fbc00d44f6b83f4
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-09-04 13:27:45 +0200

    Added simple board for RR game

--- frontend/scripts/S&L.js
+++ frontend/scripts/S&L.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
 	const SLButton = document.getElementById('S&L-play');
 
 	SLButton.addEventListener('click', function() {
+		makeboard();
 
 	});
 });
+
+function makeboard() {
+
+	const board = document.getElementById('game-board');
+	for (let i = 0; i < 16 * 16; i++) {
+		const tiles = document.createElement('div');
+		tiles.classList.add('tile');
+		board.appendChild(tiles); 
+	}
+}
