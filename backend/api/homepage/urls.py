5f2fab6e7d314244ea502aa91ad4dde2dc2781bd
ykerdel <kerdelyounes@gmail.com>
2024-10-14 18:21:12 +0200

    clean

--- backend/api/homepage/urls.py
+++ backend/api/homepage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import HomePageView, SearchUserView, UpdateUName, UpdateBio, UpdatePwd
+# from .views import HomePageView, SearchUserView, UpdateUName, UpdateBio, UpdatePwd, UpdateMail
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('home/settings/updateuname/', UpdateUName.as_view(),name='update_uname'),
     path('home/settings/updatebio/', UpdateBio.as_view(),name='update_bio'),
     path('home/settings/updatepwd/', UpdatePwd.as_view(),name='update_pwd')
+    # ,path('home/settings/updatemail/', UpdateMail.as_view(),name='update_mail')
 ]
