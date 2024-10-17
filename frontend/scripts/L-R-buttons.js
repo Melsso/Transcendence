450549b6ba754c05f7641ba240dfea6e09767c48
Sal-Zuba <alzubaidisadooon@gmail.com>
2024-10-17 11:44:52 +0200

    Added a global notification function for use call Notification("header_text", "notification_text_content", "types") types basically change the header color, it can be left empty and it will be a white one, types that exist are: alert, message, request, invite.

--- frontend/scripts/L-R-buttons.js
+++ frontend/scripts/L-R-buttons.js
@@ -31,3 +31,80 @@ function showLoginForm(logButton, regButton) {
     document.getElementById('second-reg-container').style.display = 'none';
     document.getElementById('login-form-container').style.display = 'block';
 }
+
+// const mainTwo = document.getElementById('mainTwo');
+
+// const observer = new MutationObserver(() => {
+//     if (mainTwo.offsetParent !== null) { 
+//         observer.disconnect();
+//     }
+// });
+
+// observer.observe(mainTwo, { attributes: true });
+
+function Notification(title, message, type) {
+    const mainpage = document.getElementById('mainTwo');
+
+    const main_welcome = document.createElement('div');
+    main_welcome.classList.add("position-fixed", "p-3", "top-0", "end-0");
+    main_welcome.style.zIndex = '100';
+
+    const msg_container = document.createElement('div');
+    msg_container.id = 'WELCOME';
+    msg_container.classList.add('toast');
+    msg_container.setAttribute('role', 'alert');
+    msg_container.setAttribute('aria-live', 'assertive');
+    msg_container.setAttribute('aria-atomic', 'true');
+
+    const header = document.createElement('div');
+    switch (type) {
+        case 'alert':
+            header.style.backgroundColor = 'rgba(255, 0, 0, 0.7)'; 
+            break;
+        case 'request':
+            header.style.backgroundColor = 'rgba(0, 128, 0, 0.5)'; 
+            break;
+        case 'invite':
+            header.style.backgroundColor = 'rgba(76, 39, 133, 0.5)';
+            break;
+        case 'message':
+            header.style.backgroundColor = 'rgba(25, 110, 238, 0.5)';
+            break;
+        default:
+            break;
+    }
+    header.classList.add('toast-header');
+    header.style.textAlign = 'center';
+
+    const header_msg = document.createElement('strong');
+    header_msg.classList.add('me-auto');
+    header_msg.textContent = title;
+    header_msg.style.color = 'black'; 
+
+    const msg_close = document.createElement('button');
+    msg_close.type = 'button';
+    msg_close.classList.add('btn-close');
+    msg_close.setAttribute('aria-label', 'Close');
+    msg_close.setAttribute('data-bs-dismiss', 'toast');
+
+    header.appendChild(header_msg);
+    header.appendChild(msg_close);
+
+    const msg_content = document.createElement('div');
+    msg_content.classList.add('toast-body');
+    msg_content.textContent = message;  
+
+    msg_container.appendChild(header);
+    msg_container.appendChild(msg_content);
+    main_welcome.appendChild(msg_container);
+    mainpage.appendChild(main_welcome);
+
+    const toast = new bootstrap.Toast(msg_container);
+    toast.show();
+
+    setTimeout(() => {
+        toast.hide();
+    }, 5000);
+}
+
+window.Notification = Notification;
