be9e9970fb1ba9134609bad745ffec3af44853cc
Melsso <melsopv@gmail.com>
2024-10-28 02:14:10 +0100

    Removing the game_channels app, it is no longer necessary

--- backend/api/game_channels/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .consumers import PongConsumer, ChatConsumer
-
-websocket_urlpatterns = [
-    path('ws/pong/<str:room_name>/', PongConsumer.as_asgi()),  
-    path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()), 
-]
